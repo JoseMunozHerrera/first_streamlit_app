e9e670978cce59ee858989682bee05595587206f
Jose Muñoz Herrera <rlevel@gmail.com>
2023-07-04 13:10:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,10 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+
+streamlit.subheader('Omega 3 & Blueberry Oatmeal')
+
+streamlit.subheader('Kale, Spinach & Rocket Smoothie')
+
+streamlit.subheader('Hard-Boiled Free-Range Egg')
